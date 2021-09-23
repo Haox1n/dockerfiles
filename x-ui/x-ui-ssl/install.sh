dc34cf98bee33d4ccdabca3aa9ef2ccc00c87da9
Haox1n <67004911+Haox1n@users.noreply.github.com>
2021-09-23 21:09:48 +0800

    Update install.sh

--- x-ui/x-ui-ssl/install.sh
+++ x-ui/x-ui-ssl/install.sh
@@ -120,8 +120,8 @@ case "$START_INPUT" in
 
 accout_conf (){
 WORK_PATH=$(dirname $(readlink -f $0))
-wget https://raw.githubusercontent.com/stilleshan/dockerfiles/main/x-ui/x-ui-ssl/x-ui-ssl.tar
-tar -xvf x-ui-ssl.tar
+#wget https://raw.githubusercontent.com/stilleshan/dockerfiles/main/x-ui/x-ui-ssl/x-ui-ssl.tar
+#tar -xvf x-ui-ssl.tar
 cat >${WORK_PATH}/x-ui-ssl/conf/account.conf<<EOF
 export ${API_ID_HEADER}="${API_ID_INPUT}"
 export ${API_KEY_HEADER}="${API_KEY_INPUT}"
